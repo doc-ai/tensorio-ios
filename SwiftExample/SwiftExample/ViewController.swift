29bf6c87e2b864f875e69bd672d28861d2013a23
phildow <phil@phildow.net>
2019-02-13 17:54:48 -0800

    initial swift example

---
+++ SwiftExample/SwiftExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftExample
+//
+//  Created by Phil Dow on 2/13/19.
+//  Copyright © 2019 doc.ai. All rights reserved.
+//
+
+import UIKit
+import TensorIO
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
