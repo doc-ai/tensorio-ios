703ad9783b8c1aa078aafedcb58275ed2781e397
phildow <phil@phildow.net>
2019-02-13 18:40:44 -0800

    added swift example

--- SwiftExample/SwiftExample/ViewController.swift
+++ SwiftExample/SwiftExample/ViewController.swift
@@ -11,11 +11,28 @@ import TensorIO
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var infoView: ResultInfoView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "example-image")!
+        let pixels = image.pixelBuffer()!
+        let value = pixels.takeUnretainedValue() as CVPixelBuffer
+        let buffer = TIOPixelBuffer(pixelBuffer:value, orientation: .up)
+        
+        let path = Bundle.main.path(forResource: "mobilenet_v2_1.4_224", ofType: "tfbundle", inDirectory: "models")!
+        let model = TIOTFLiteModel.withBundleAtPath(path)!
+        
+        let classification = model.run(on: buffer)
+        // print(classification)
+        
+        let top5 = ((classification as! NSDictionary)["classification"] as! NSDictionary).topN(5, threshold: 0.1)
+        print(top5)
+        
+        imageView.image = image
+        infoView.classifications = (top5 as NSDictionary).description
     }
 
-
 }
-
