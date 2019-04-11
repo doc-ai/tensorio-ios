f81d34b022d9819a2834d597ec8cf2c58fa5aa6c
phildow <phil@phildow.net>
2019-04-11 15:27:30 -0700

    create pixel buffer from output tensor, needs test

--- TensorIO/Classes/TensorFlow/TIOTensorFlowData/TIOPixelBuffer+TIOTensorFlowData.mm
+++ TensorIO/Classes/TensorFlow/TIOTensorFlowData/TIOPixelBuffer+TIOTensorFlowData.mm
@@ -22,6 +22,25 @@
 #import "TIOPixelBufferLayerDescription.h"
 #import "TIOVisionPipeline.h"
 
+/**
+ * Copies a pixel buffer in ARGB or BGRA format to a tensor.
+ *
+ * The pixel buffer must already be in the shape and format expected by the input tensor,
+ * with the shape parameter describing its dimensions. The alpha channel will be ignored.
+ *
+ * If a normalizer is provided then the pixel buffer's values will be scaled using the
+ * normalizer.
+ *
+ * `tensor_t` will be `float_t` (32 bits) for an unquantized model or `uint8_t` (8 bits)
+ * for a quantized model.
+ *
+ * @param pixelBuffer The pixel buffer that will be copied to the tensor.
+ * @param tensor The tensor that will receive the pixel buffer values.
+ * @param shape The shape, i.e. width, height, and number of channels of the tensor.
+ * @param normalizer A scaling function that will be applied to the pixel values as
+ * they are copied to the tensor. May be `nil`.
+ */
+
 template <typename T>
 void TIOCopyCVPixelBufferToTensorFlowTensor(CVPixelBufferRef pixelBuffer, tensorflow::Tensor tensor, TIOImageVolume shape, _Nullable TIOPixelNormalizer normalizer) {
     auto image_mapped = tensor.tensor<T, 4>();
@@ -72,6 +91,102 @@ void TIOCopyCVPixelBufferToTensorFlowTensor(CVPixelBufferRef pixelBuffer, tensor
     CFRelease(pixelBuffer);
 }
 
+// TODO: ensure 16 byte pixel buffer alignment
+
+/**
+ * Copies tensor bytes directly into a pixel buffer from a tensor, applying a denormalization
+ * function and adjusting for the pixel format.
+ *
+ * The resulting pixel buffer will (eventually) be 16 byte aligned. The caller must release the
+ * pixelBuffer with `CVPixelBufferRelease`.
+ *
+ * @param pixelBuffer A pointer to the pixel buffer that will be filled with the transformed tensor data
+ * @param tensor A pointer to the tensor that contains the image data
+ * @param shape The width, height, and number of channels of the tensor. Number of channels should be three.
+ * @param pixelFormat The format of the tensor image data, must be kCVPixelFormatType_32ARGB or kCVPixelFormatType_32BGRA.
+ * Note that the alpha channel is ignored.
+ * @param denormalizer A function that can convert the tensor image data to pixel values, may be `nil`.
+ *
+ * @return CVReturn `kCVReturnSuccess` if the operation was successful, some other value if not
+ */
+
+template <typename T>
+CVReturn TIOCreateCVPixelBufferFromTensorFlowTensor(_Nonnull CVPixelBufferRef * _Nonnull pixelBuffer, tensorflow::Tensor tensor, TIOImageVolume shape, OSType pixelFormat, _Nullable TIOPixelDenormalizer denormalizer) {
+    
+    assert( pixelFormat == kCVPixelFormatType_32ARGB || pixelFormat == kCVPixelFormatType_32BGRA );
+    assert( shape.width % 16 == 0);
+    
+    const int tensor_channels = shape.channels;
+    const int image_width = shape.width;
+    const int image_height = shape.height;
+    const int bytes_per_row = shape.width * 4;
+    const int image_channels = 4; // by definition (ARGB, BGRA)
+    
+    CVPixelBufferRef outputBuffer = NULL;
+    
+    CVReturn status = CVPixelBufferCreate(
+        kCFAllocatorDefault,
+        image_width,
+        image_height,
+        pixelFormat,
+        NULL,
+        &outputBuffer);
+    
+    // Error handling
+    
+    if ( status != kCVReturnSuccess ) {
+        NSLog(@"Couldn't create pixel buffer");
+        return status;
+    }
+    
+    // Copy the pixel data
+    
+    // channel_offset is used to skip the alpha channel when copying to the tensor
+    // it is 1 for ARGB images and 0 for BGRA images.
+    
+    CVPixelBufferLockBaseAddress(outputBuffer, kNilOptions);
+    
+    const int channel_offset = pixelFormat == kCVPixelFormatType_32ARGB
+        ? 1
+        : 0;
+    
+    const int alpha_channel = pixelFormat == kCVPixelFormatType_32ARGB
+        ? 0
+        : 3;
+    
+    auto image_mapped = tensor.tensor<T, 4>();
+    uint8_t* out_addr = (uint8_t*)CVPixelBufferGetBaseAddress(outputBuffer);
+    
+    if ( denormalizer == nil ) {
+        for (int y = 0; y < image_height; y++) {
+            for (int x = 0; x < image_width; x++) {
+                auto* out_pixel = out_addr + (y * bytes_per_row) + (x * image_channels);
+                for (int c = 0; c < tensor_channels; ++c) {
+                    out_pixel[c+channel_offset] = image_mapped(0, y, x, c);
+                }
+                out_pixel[alpha_channel] = 255;
+            }
+        }
+    } else {
+        for (int y = 0; y < image_height; y++) {
+            for (int x = 0; x < image_width; x++) {
+                auto* out_pixel = out_addr + (y * bytes_per_row) + (x * image_channels);
+                for (int c = 0; c < tensor_channels; ++c) {
+                    out_pixel[c+channel_offset] = denormalizer(image_mapped(0, y, x, c), c);
+                }
+                out_pixel[alpha_channel] = 255;
+            }
+        }
+    }
+    
+    // Clean up
+    
+    CVPixelBufferUnlockBaseAddress(outputBuffer, kNilOptions);
+    *pixelBuffer = outputBuffer;
+    
+    return kCVReturnSuccess;
+}
+
 // MARK: -
 
 @interface TIOPixelBuffer (TIOTensorFlowData_Protected)
@@ -84,8 +199,35 @@ void TIOCopyCVPixelBufferToTensorFlowTensor(CVPixelBufferRef pixelBuffer, tensor
 
 - (nullable instancetype)initWithTensor:(tensorflow::Tensor)tensor description:(id<TIOLayerDescription>)description {
     assert([description isKindOfClass:TIOPixelBufferLayerDescription.class]);
-    #warning implement
-    return nil;
+    
+    TIOPixelBufferLayerDescription *pixelBufferDescription = (TIOPixelBufferLayerDescription*)description;
+    CVPixelBufferRef pixelBuffer = NULL;
+    CVReturn result;
+    
+    if ( description.isQuantized ) {
+        result = TIOCreateCVPixelBufferFromTensorFlowTensor<uint8_t>(
+            &pixelBuffer,
+            tensor,
+            pixelBufferDescription.shape,
+            pixelBufferDescription.pixelFormat,
+            pixelBufferDescription.denormalizer
+        );
+    } else {
+        result = TIOCreateCVPixelBufferFromTensorFlowTensor<float_t>(
+            &pixelBuffer,
+            tensor,
+            pixelBufferDescription.shape,
+            pixelBufferDescription.pixelFormat,
+            pixelBufferDescription.denormalizer
+        );
+    }
+    
+    if ( result != kCVReturnSuccess ) {
+        NSLog(@"There was a problem creating the pixel buffer from the tensor");
+        return nil;
+    }
+    
+    return [self initWithPixelBuffer:pixelBuffer orientation:kCGImagePropertyOrientationUp];
 }
 
 - (tensorflow::Tensor)tensorWithDescription:(id<TIOLayerDescription>)description {
