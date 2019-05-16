a1bdca59c041005406a57deefddd6224c855d91c
phildow <phil@phildow.net>
2019-05-15 18:24:02 -0700

    add live test setup script

--- DeployExample/DeployExample/ViewController.m
+++ DeployExample/DeployExample/ViewController.m
@@ -35,22 +35,67 @@
     NSURL *URL = [NSURL URLWithString:@"https://tio-models-test.dev.docai.beer/rest/v1/repository"];
     self.repository = [[TIOModelRepository alloc] initWithBaseURL:URL session:nil];
     
-    [self.repository GETHealthStatus:^(TIOMRStatus * _Nullable response, NSError * _Nonnull error) {
-        if (error) {
-            NSLog(@"There was an error getting status");
-            return;
-        }
-        
-        NSLog(@"HEALTH STATUS: %lu", (unsigned long)response.status);
-    }];
+//    [self.repository GETHealthStatus:^(TIOMRStatus * _Nullable response, NSError * _Nonnull error) {
+//        if (error) {
+//            NSLog(@"There was an error getting status");
+//            return;
+//        }
+//
+//        NSLog(@"HEALTH STATUS: %lu", (unsigned long)response.status);
+//    }];
     
-    [self.repository GETModels:^(TIOMRModels * _Nullable response, NSError * _Nullable error) {
+//    [self.repository GETModels:^(TIOMRModels * _Nullable response, NSError * _Nullable error) {
+//        if (error) {
+//            NSLog(@"There was an error getting models");
+//            return;
+//        }
+//
+//        NSLog(@"MODELS: %@", response.modelIds);
+//    }];
+
+//    [self.repository GETModelWithId:@"TestModel-1557962403" callback:^(TIOMRModel * _Nullable model, NSError * _Nullable error) {
+//        if (error) {
+//            NSLog(@"There was an error getting model");
+//            return;
+//        }
+//
+//        NSLog(@"MODEL: %@", model);
+//    }];
+    
+//    [self.repository GETHyperparametersForModelWithId:@"TestModel-1557962403" callback:^(TIOMRHyperparameters * _Nullable hyperparameters, NSError * _Nullable error) {
+//        if (error) {
+//            NSLog(@"There was an error getting model");
+//            return;
+//        }
+//
+//        NSLog(@"HYPERPARAMETERS: %@", hyperparameters.hyperparametersIds);
+//    }];
+    
+//    [self.repository GETHyperparameterForModelWithId:@"TestModel-1557962403" hyperparametersId:@"hyperparameters-1" callback:^(TIOMRHyperparameter * _Nullable hyperparameter, NSError * _Nullable error) {
+//        if (error) {
+//            NSLog(@"There was an error getting model");
+//            return;
+//        }
+//
+//        NSLog(@"HYPERPARAMETER: %@", hyperparameter.hyperparametersId);
+//    }];
+    
+//    [self.repository GETCheckpointsForModelWithId:@"TestModel-1557962403" hyperparametersId:@"hyperparameters-1" callback:^(TIOMRCheckpoints * _Nullable checkpoints, NSError * _Nullable error) {
+//        if (error) {
+//            NSLog(@"There was an error getting model");
+//            return;
+//        }
+//
+//        NSLog(@"CHECKPOINTS: %@", checkpoints.checkpointIds);
+//    }];
+    
+    [self.repository GETCheckpointForModelWithId:@"TestModel-1557962403" hyperparametersId:@"hyperparameters-1" checkpointId:@"checkpoint-1" callback:^(TIOMRCheckpoint * _Nullable checkpoint, NSError * _Nullable error) {
         if (error) {
-            NSLog(@"There was an error getting models");
+            NSLog(@"There was an error getting model");
             return;
         }
-        
-        NSLog(@"MODELS: %@", response.modelIds);
+
+        NSLog(@"CHECKPOINT: %@", checkpoint.checkpointId);
     }];
 }
 
