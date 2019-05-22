ba4584a0f1efc28fc29c47469235b414e2fbd41c
phildow <phil@phildow.net>
2019-05-22 12:04:54 -0700

    begin to spec out the federated manager

--- TensorIO/Classes/Federated/TIOFederatedManager.h
+++ TensorIO/Classes/Federated/TIOFederatedManager.h
@@ -22,14 +22,74 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol TIOFederatedManagerDataSourceProvider;
+@protocol TIOFederatedManagerDelegate;
+
 /**
- * Allows users to register for or request learning tasks from a TensorIO Flea
+ * Allows users to register for or request federated tasks from a TensorIO Flea
  * server and provide data to the model for those task. The
- * `TIOFederatedManager` handles coordinates between learning tasks,
- * client-server interactions, and actually executing training on a model.
+ * `TIOFederatedManager` coordinates between learning tasks, client-server
+ * interactions, and actually executing training on a model.
+ *
+ * You must set a data source provider prior to registering for or requesting new
+ * tasks. The data source provider vends instances of `TIOBatchDataSource` to
+ * the manager, which are themselves responsible for providing batch data for
+ * a task. You may return the data source provider itself if it also knows how
+ * to vend data, but it is then that classes responsibility to only vend the
+ * correct data for a specific task.
  */
 
 @interface TIOFederatedManager : NSObject
+
+/**
+ * A convenience initializer for instantiating a federated manager with a data
+ * source provider and delegate;
+ */
+
+- (instancetype)initWithDataSourceProvider:(id<TIOFederatedManagerDataSourceProvider>)dataSourceProvider delegate:(nullable id<TIOFederatedManagerDelegate>)delegate;
+
+/**
+ * The data source provider is responsible for vending instances of
+ * `TIOBatchDataSource` to the manager for the specified tasks. It may be the
+ * provider itself.
+ */
+
+@property (weak, readwrite) id<TIOFederatedManagerDataSourceProvider> dataSourceProvider;
+
+/**
+ * The delegate will be notified of federated task events as they occur.
+ */
+
+@property (weak, nullable, readwrite) id<TIOFederatedManagerDelegate> delegate;
+
+/**
+ * The model ids that are being tracked for tasks against a TensorIO-Flea repository
+ */
+
+@property (readonly) NSArray<NSString*> *registeredModelIds;
+
+/**
+ * Informs the manager to check for tasks for a model with a given id whenever
+ * `checkForTasks:` is called. You must call that method in addition to registering
+ * specific models.
+ */
+
+- (void)registerForTasksForModelWithId:(NSString*)modelId;
+
+/**
+ * Unregisters a model id with the manager.
+ */
+
+- (void)unregisterForTasksForModelWithId:(NSString*)modelId;
+
+/**
+ * Begins the process of requesting tasks for the registered models from a
+ * TensorIO-Flea server. This will set of a chain of network calls on a background
+ * threat that may include API requests, downloading model updates and task
+ * bundles, and finally sending the results of a task back to the Flea server.
+ */
+
+- (void)checkForTasks;
 
 @end
 
--- TensorIO/Classes/Federated/TIOFederatedManager.m
+++ TensorIO/Classes/Federated/TIOFederatedManager.m
@@ -19,7 +19,68 @@
 //
 
 #import "TIOFederatedManager.h"
+#import "TIOFederatedManagerDataSourceProvider.h"
+#import "TIOFederatedManagerDelegate.h"
+#import "TIOFederatedTaskBundle.h"
+#import "TIOModelBundle.h"
+#import "TIOModel.h"
+#import "TIOTrainableModel.h"
+#import "TIOModelTrainer.h"
+#import "TIOFederatedTask.h"
+#import "TIOModelTrainer+FederatedTask.h"
+#import "TIOBatchDataSource.h"
 
 @implementation TIOFederatedManager
+
+- (instancetype)initWithDataSourceProvider:(id<TIOFederatedManagerDataSourceProvider>)dataSourceProvider delegate:(nullable id<TIOFederatedManagerDelegate>)delegate {
+    if ((self=[self init])) {
+        _dataSourceProvider = dataSourceProvider;
+        _delegate = delegate;
+    }
+    return self;
+}
+
+- (void)registerForTasksForModelWithId:(NSString*)modelId {
+    [[self mutableArrayValueForKey:@"registeredModelIds"] addObject:modelId];
+}
+
+
+- (void)unregisterForTasksForModelWithId:(NSString*)modelId {
+    [[self mutableArrayValueForKey:@"registeredModelIds"] removeObject:modelId];
+}
+
+- (void)checkForTasks {
+    assert(self.dataSourceProvider != nil);
+    
+    // For each registered model id:
+    
+    // : Check if there are any tasks available for it
+    
+    // : If there are tasks, download the task bundle
+    
+    // : If the model is not available, download the model
+    
+    // : [ If the model has an update, update the model - or earlier? ]
+    
+    // : Given a task bundle and model bundle:
+    
+    // :: Request a data source from the data source provider
+    
+    // :: Execute the task with a trainer, model, and data source
+    
+    // :: Return the results of the task to the server
+}
+
+- (id<TIOData>)executeTask:(TIOFederatedTask*)task model:(id<TIOTrainableModel>)model {
+    id<TIOBatchDataSource> dataSource = [self dataSourceForTask:task];
+    TIOModelTrainer *trainer = [[TIOModelTrainer alloc] initWithModel:model task:task dataSource:dataSource];
+    id<TIOData> results = [trainer train];
+    
+    return results;
+}
+
+- (id<TIOBatchDataSource>)dataSourceForTask:(TIOFederatedTask*)task {
+    return [self.dataSourceProvider dataSourceForTaskWithId:task.identifier];
+}
 
 @end
