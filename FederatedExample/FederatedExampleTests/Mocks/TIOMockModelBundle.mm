51d505a23c8bdd8522f7d63a4f9f1411f3ab4efb
phildow <phil@phildow.net>
2019-05-28 20:00:36 -0700

    mock model bundle vends mock model

--- FederatedExample/FederatedExampleTests/Mocks/TIOMockModelBundle.mm
+++ FederatedExample/FederatedExampleTests/Mocks/TIOMockModelBundle.mm
@@ -3,11 +3,36 @@
 //  FederatedExampleTests
 //
 //  Created by Phil Dow on 5/28/19.
-//  Copyright © 2019 doc.ai. All rights reserved.
+//  Copyright © 2019 doc.ai (http://doc.ai)
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "TIOMockModelBundle.h"
 
-@implementation TIOMockModelBundle
+@implementation TIOMockModelBundle {
+    id<TIOModel> _mockedModel;
+}
+
+- (instancetype)initWithMockedModel:(id<TIOModel>)model path:(NSString*)path {
+    if ((self=[self initWithPath:path])) {
+        _mockedModel = model;
+    }
+    return self;
+}
+
+- (nullable id<TIOModel>)newModel {
+    return _mockedModel;
+}
 
 @end
