d7b9292e5edcd5de7eae78333b9abf1267513eb8
phildow <phil@phildow.net>
2019-07-15 13:22:44 -0700

    confirm with a full swift tensorflow test

---
+++ SwiftTensorFlowExample/README.md
@@ -0,0 +1,9 @@
+## Swift TensorFlow Example
+
+Full support for training and inference with TensorFlow in Swift.
+
+### Installation Notes
+
+Run `pod install` in this directory and then follow the Version 0.2.0 instructions at [tensorio-tensorflow-ios](https://github.com/doc-ai/tensorio-tensorflow-ios) to replace the TensorFlow binary with the latest one.
+
+Use *SwiftTensorFlowExample.xcworkspace* to run the example.
--- SwiftTensorFlowExample/SwiftTensorFlowExampleTests/SwiftTensorFlowExampleTests.swift
+++ SwiftTensorFlowExample/SwiftTensorFlowExampleTests/SwiftTensorFlowExampleTests.swift
@@ -7,28 +7,127 @@
 //
 
 import XCTest
+import TensorIO
 @testable import SwiftTensorFlowExample
 
 class SwiftTensorFlowExampleTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let testModelsDir = Bundle.main.path(forResource: "models-tests", ofType: nil)!
+    
+    func bundle(named name: String) -> TIOModelBundle? {
+        let path = URL(fileURLWithPath: testModelsDir).appendingPathComponent(name).path
+        return TIOModelBundle(path: path)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func model(with bundle: TIOModelBundle) -> TIOModel? {
+        guard let model = bundle.newModel() else {
+            print("There was a problem instantiating the model from the bundle")
+            return nil
         }
+        
+        guard let _ = try? model.load() else {
+            print("There was a problem loading the model")
+            return nil
+        }
+        
+        return model
     }
+    
+    // MARK: -
+    
+    override func setUp() { }
+    override func tearDown() { }
 
+    // MARK: - Swift Sanity Checks
+
+    func testCatsVsDogsPredict() {
+        
+        let image = UIImage(named: "cat.jpg")!
+        let pixels = image.pixelBuffer()!
+        let value = pixels.takeUnretainedValue() as CVPixelBuffer
+        let buffer = TIOPixelBuffer(pixelBuffer: value, orientation: .up)
+        
+        guard let bundle = self.bundle(named: "cats-vs-dogs-predict.tiobundle"),
+              let model = self.model(with: bundle) else {
+            XCTFail()
+            return
+        }
+        
+        let classification = model.run(on: buffer, error: nil)
+        XCTAssertNotNil(classification)
+        
+        let sigmoid = ((classification as! NSDictionary)["sigmoid"] as! Float)
+        XCTAssertNotNil(sigmoid)
+    }
+    
+    func testCatsVsDogsTrainWithPlaceholders() {
+
+        guard let bundle = self.bundle(named: "cats-vs-dogs-train-with-placeholder.tiobundle"),
+              let model = self.model(with: bundle) as? TIOTrainableModel else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertNotNil(bundle);
+        XCTAssertNotNil(model);
+       
+        // Prepare Data
+       
+        let cat = TIOPixelBuffer(pixelBuffer: UIImage(named: "cat.jpg")!.pixelBuffer()!.takeUnretainedValue() as CVPixelBuffer, orientation: .up)
+        let dog = TIOPixelBuffer(pixelBuffer: UIImage(named: "dog.jpg")!.pixelBuffer()!.takeUnretainedValue() as CVPixelBuffer, orientation: .up)
+        
+        // labels: 0=cat, 1=dog
+        
+        let batch = TIOBatch(keys: ["image", "labels"])
+        
+        batch.addItem([
+            "image": cat,
+            "labels": 0 as NSNumber
+        ])
+        
+        batch.addItem([
+            "image": dog,
+            "labels": 1 as NSNumber
+        ])
+        
+        // Train with two sets of placeholder values and test that changing the placeholder
+        // actually has an effect
+        
+        let placeholders1 = [
+            "placeholder_adam_learning_rate": 0.0001 as NSNumber
+        ]
+    
+        let placeholders2 = [
+            "placeholder_adam_learning_rate": 0.001 as NSNumber
+        ]
+        
+        var results1: [Float] = []
+        var results2: [Float] = []
+        
+        for _ in 1...10 {
+            var error: NSError?
+            let results = model.train(batch, placeholders: placeholders1, error: &error) as! NSDictionary
+            let loss = results["sigmoid_cross_entropy_loss/value"]
+            
+            XCTAssertNil(error);
+            XCTAssertNotNil(loss); // at epoch 0 ~ 0.2232
+            XCTAssert(loss is NSNumber);
+            
+            results1.append(loss as! Float)
+        }
+        
+        for _ in 1...10 {
+            var error: NSError?
+            let results = model.train(batch, placeholders: placeholders2, error: &error) as! NSDictionary
+            let loss = results["sigmoid_cross_entropy_loss/value"]
+            
+            XCTAssertNil(error);
+            XCTAssertNotNil(loss); // at epoch 0 ~ 0.2232
+            XCTAssert(loss is NSNumber);
+            
+            results2.append(loss as! Float)
+        }
+        
+        XCTAssertNotEqual(results1, results2)
+    }
 }
