2a540548061dc2b69c51fc4937172467678c5bd6
phildow <phil@phildow.net>
2019-07-31 11:43:54 -0700

    remove deploy and federated from run tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,5 @@ xcodebuild clean build test -enableCodeCoverage YES -workspace Example/TensorIO.
 xcodebuild clean build test -enableCodeCoverage YES -workspace TensorFlowExample/TensorFlowExample.xcworkspace -scheme TensorFlowExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' | xcpretty
 xcodebuild clean build test -enableCodeCoverage YES -workspace SwiftExample/SwiftExample.xcworkspace -scheme SwiftExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' | xcpretty
 xcodebuild clean build test -enableCodeCoverage YES -workspace SwiftTensorFlowExample/SwiftTensorFlowExample.xcworkspace -scheme SwiftTensorFlowExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' | xcpretty
-xcodebuild clean build test -enableCodeCoverage YES -workspace DeployExample/DeployExample.xcworkspace -scheme DeployExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' | xcpretty
-xcodebuild clean build test -enableCodeCoverage YES -workspace FederatedExample/FederatedExample.xcworkspace -scheme FederatedExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' | xcpretty
 
 # pod lib lint --skip-import-validation --allow-warnings
