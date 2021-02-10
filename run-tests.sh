76806a6a9f6436a3277b2166d60d97e52cac02c7
Philip Dow <phil@phildow.net>
2021-02-10 12:51:42 -0600

    bump versions, pod install

--- run-tests.sh
+++ run-tests.sh
@@ -2,9 +2,9 @@
 
 set -Eeuo pipefail
 
-xcodebuild clean build test -enableCodeCoverage YES -workspace Example/TensorIO.xcworkspace -scheme TensorIO-Example -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4' | xcpretty
-xcodebuild clean build test -enableCodeCoverage YES -workspace TensorFlowExample/TensorFlowExample.xcworkspace -scheme TensorFlowExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4' | xcpretty
-xcodebuild clean build test -enableCodeCoverage YES -workspace SwiftExample/SwiftExample.xcworkspace -scheme SwiftExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4' | xcpretty
-xcodebuild clean build test -enableCodeCoverage YES -workspace SwiftTensorFlowExample/SwiftTensorFlowExample.xcworkspace -scheme SwiftTensorFlowExample -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4' | xcpretty
+xcodebuild clean build test -enableCodeCoverage YES -workspace Example/TensorIO.xcworkspace -scheme TensorIO-Example -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.3' | xcpretty
+xcodebuild clean build test -enableCodeCoverage YES -workspace TensorFlowExample/TensorFlowExample.xcworkspace -scheme TensorFlowExample -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.3' | xcpretty
+xcodebuild clean build test -enableCodeCoverage YES -workspace SwiftExample/SwiftExample.xcworkspace -scheme SwiftExample -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.3' | xcpretty
+xcodebuild clean build test -enableCodeCoverage YES -workspace SwiftTensorFlowExample/SwiftTensorFlowExample.xcworkspace -scheme SwiftTensorFlowExample -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.3' | xcpretty
 
 # pod lib lint --skip-import-validation --allow-warnings
